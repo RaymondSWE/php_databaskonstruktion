9ed00395a7552d80f5ac740dda5897aef09be4ee
Raman Mohammed <RamanMhoammed@hotmail.com>
2022-10-19 19:46:34 +0200

    idk, things works now

---
+++ db.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Databas test</title>
+</head>
+<body>
+    <?php
+    function debug($o)
+    {
+        echo '<pre>';
+        print_r($o);
+        echo '</pre>';
+    }
+    
+    debug($_POST);
+    
+    $pdo = new PDO('mysql:dbname=a21rammo;host=localhost', 'raman', 'user_password');
+    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+?>
+
+
+<h2>Kid Table</h2>
+<table border='1'>
+<?php
+		foreach($pdo->query("SELECT * FROM kid") as $row){
+			echo "<tr>";
+			echo "<td>".$row['PNR']."</td>";			
+			echo "<td>".$row['name']."</td>";			
+			echo "<td>".$row['birthday']."</td>";
+            echo "<td>".$row['disobedience']."</td>";
+            echo "<td>".$row['deliveryNr']."</td>";
+            echo "<td>".$row['type']."</td>";
+			echo "</tr>";
+		}		
+?>
+</table>
+
+
+<h3>Kid Option Box</h3>
+<form action="db.php" method="post">
+	<select size='1' name='PNR'>
+<?php		
+		foreach($pdo->query( 'SELECT * FROM kid ORDER BY NAME;' ) as $row){
+			echo '<option value="'.$row['PNR'].'">';
+			echo $row['name'];			
+			echo '</option>';
+		}		
+?>
+   </select>
+   <input type="submit" value="Send">
+   <input type="reset">
+</form>
+
+
+
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,165 +11,64 @@
 <body>
     <h3>DB</h3>
     <?php
-    function debug($o)
-    {
-        echo '<pre>';
-        print_r($o);
-        echo '</pre>';
-    }
+function debug($o)
+{
+    echo '<pre>';
+    print_r($o);
+    echo '</pre>';
+}
 
-    debug($_POST);
+debug($_POST);
 
-    $pdo = new PDO('mysql:dbname=a21rammo;host=localhost', 'raman', 'user_password');
+$pdo = new PDO('mysql:dbname=a21rammo;host=localhost', 'raman', 'user_password');
+$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+?>
 
-    // foreach($pdo -> query( 'SELECT * FROM KID;') as $row) {
-    //     debug($row);
-    // }
-    if (isset($_POST['toy'])) {
-        $query = 'SELECT * FROM toy WHERE toyId=:toyId;';
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':toyId', $_POST['name']);
-        $stmt->execute();
-        foreach ($stmt as $key->$row) {
-            echo '<div>' . $row['toyId'] . '</div>';
-            echo '<div>' . $row['toyName'] . '</div>';
-            echo '<div>' . $row['toySizeCM'] . '</div>';
-            echo '<div>' . $row['magicPercentage'] . '</div>';
-            echo '<div>' . $row['toyPriceSEK'] . '</div>';
-        }
-    }
-
-    if (isset($_POST['PNR'])) {
-        $querystring = 'INSERT INTO kid (PNR, name, birthday, disobedience, deliveryNr, type) VALUES(:PNR,:name,:birthday,:disobedience,:deliveryNr,:type);';
-        $stmt = $pdo->prepare($querystring);
-        $stmt->bindParam(':PNR', $_POST['PNR']);
-        $stmt->bindParam(':name', $_POST['name']);
-        $stmt->bindParam(':disobedience', $_POST['disobedience']);
-        $stmt->bindParam(':deliveryNr', $_POST['deliveryNr']);
-        $stmt->bindParam(':type', $_POST['type']);
-    }
-
-    // If U_PNR is true, update
-    if (isset($_POST['PNR'])) {
-        $querystring = 'UPDATE kid SET disobedience=:disobedience WHERE PNR=:PNR;';
-        $stmt = $pdo->prepare($querystring);
-        $stmt->bindParam(':PNR', $_POST['u_PNR']);
-        $stmt->bindParam(':name', $_POST['u_disobedience']);
-
-    }
+<h2>Wishlist Table</h2>
+<table border='1'>
+<?php
+		foreach($pdo->query("SELECT * FROM wishlist") as $row){
+			echo "<tr>";
+			echo "<td>".$row['year']."</td>";			
+			echo "<td>".$row['PNR']."</td>";			
+			echo "<td>".$row['toyId']."</td>";
+            echo "<td>".$row['description']."</td>";
+            echo "<td>".$row['conceded']."</td>";
+            echo "<td>".$row['delivered']."</td>";
+			echo "</tr>";
+		}		
+?>
+</table>
 
 
-    echo '<ul>';
-    foreach ($pdo->query('SELECT * FROM KID;') as $row) {
-        echo '<li>' . $row['name'] . ', ' . $row['type'] . '</li>';
-    }
-    echo '</ul>';
 
-    echo '<ul>';
-    foreach ($pdo->query('SELECT * FROM toy;') as $row) {
-        echo '<li>' . $row['toyId'] . ', ' . $row['toyName'] . '</li>';
-    }
-    echo '</ul>';
+<h3>Add new kid</h3>
+<form action="index.php" method="post">
+PNR: <input type="text" name="PNR" /><br>
+Name: <input type="text" name="name" /><br>
+Birthday: <input type="text" name="birthday" /><br>
+Disobedience: <input type="text" name="disobedience" /><br>
+Delivery number: <input type="text" name="deliveryNr" /><br>
+Type: <input type="text" name="type" /><br>
+<input type="submit" />
+</form> 
+<table>
 
-    ?>
-    <form action="index.php" method="POST">
-        <select size='1' name='kid'>
-            <?php
-            foreach ($pdo->query('SELECT * FROM kid;') as $row) {
-                echo '<option value="' . $row['PNR'] . '">' . $row['name'] . '</option>';
-            }
-            ?>
-        </select>
-        <input type="submit" value="Show SSN">
-    </form>
-
-
-    <?php
-    // Data query to our database
-    echo '<div>';
-    if (isset($_POST['kid'])) {
-        $query = ' SELECT * FROM kid WHERE pnr=:pnr;';
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':pnr', $_POST['pnr']);
-        $stmt->execute();
-        foreach ($stmt as $key->$row) {
-            echo '<div>' . $row['pnr'] . '</div>';
-            echo '<div>' . $row['birthday'] . '</div>';
-            echo '<div>' . $row['type'] . '</div>';
-            echo '<div>' . $row['birthday'] . '</div>';
-        }
-    } else {
-        echo '<span>Choose a kid</span>';
-    }
-    echo '</div>';
-    ?>
-
-    <?php
-    if (isset($_POST['PNR'])) {
-        $querystring = 'INSERT INTO kid (PNR, name, birthday, disobedience, deliveryNr, type) VALUES(:PNR,:name,:birthday,:disobedience,:deliveryNr,:type);';
-        $stmt = $pdo->prepare($querystring);
-        $stmt->bindParam(':PNR', $_POST['PNR']);
-        $stmt->bindParam(':name', $_POST['name']);
-        $stmt->bindParam(':disobedience', $_POST['disobedience']);
-        $stmt->bindParam(':deliveryNr', $_POST['deliveryNr']);
-        $stmt->bindParam(':type', $_POST['type']);
-    }
-
-
-    ?>
-
-    <form action="index.php" method="post">
-        <input type="text" name"PNR" placeholder="PNR"><br>
-        <input type="text" name"name" placeholder="name"><br>
-        <input type="text" name"birthday" placeholder="birthday"><br>
-        <input type="text" name"disobedience" placeholder="disobedience"><br>
-        <input type="text" name"deliveryNr" placeholder="deliveryNr"><br>
-        <input type="text" name"type" placeholder="type"><br>
-        <input type="submit" value="add kid">
-    </form>
-
-    <?php
-    // Data query to our database
-    if (isset($_POST['toy'])) {
-        $query = ' SELECT * FROM toy WHERE toyId=:toyId;';
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':toyId', $_POST['toys']);
-        $stmt->execute();
-        foreach ($stmt as $key->$row) {
-            echo '<div>' . $row['toyId'] . '</div>';
-            echo '<div>' . $row['toyName'] . '</div>';
-        }
-    }
-    ?>
-
-    <form action="index.php" method="POST">
-        <select size='1' name='toy'>
-            <?php
-            foreach ($pdo->query('SELECT * FROM toy;') as $row) {
-                echo '<option value="' . $row['toyId'] . '">' . $row['toyName'] . '</option>';
-            }
-            ?>
-        </select>
-        <input type="submit" value="Show toy information">
-    </form>
-
-    <?php 
-    // Uppdate kids disobedience
-            $query = 'SELECT * FROM kid WHERE PNR=:PNR;';
-            $stmt = $pdo -> prepare($query);
-            $stmt -> bindParam(':PNR', $_POST['PNR']);
-            $stmt ->execute();
-            foreach($stmt as $key -> $row) {
-                echo '<div>'.$row['name'].'</div>';
-                echo '<form action="index.php" method="post">';
-                echo '<input type="hidden" name="u_PNR" value="'.$row['PNR'].'"><br>';
-                echo '<input type="text" name="u_disobedience" value="'.$row['disobedience'].'"><br>';
-                echo '<input type="submit" value="Uppdate disobedience of kid">';
-                echo '</form>';
-            }
-    
-    ?>
-
+<?php
+		// Only make insert if there is a form post to process
+		if(isset($_POST['PNR'])){
+				$querystring='INSERT INTO kid (PNR,name,birthday,disobedience,deliveryNr,type) VALUES(:PNR,:name,:birthday,:disobedience,:deliveryNr,:type);';
+				$stmt = $pdo->prepare($querystring);
+				$stmt->bindParam(':PNR', $_POST['PNR']);
+				$stmt->bindParam(':name', $_POST['name']);
+				$stmt->bindParam(':birthday', $_POST['birthday']);
+                $stmt->bindParam(':disobedience', $_POST['disobedience']);
+                $stmt->bindParam(':deliveryNr', $_POST['deliveryNr']);
+                $stmt->bindParam(':type', $_POST['type']);
+				$stmt->execute();
+		}
+?>
+</table>
 </body>
 
 </html>
