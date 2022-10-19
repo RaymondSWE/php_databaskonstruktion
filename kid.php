5395ace1afe2436008eca440bb07c1ca98d1370e
Raman Mohammed <RamanMhoammed@hotmail.com>
2022-10-19 23:40:55 +0200

    added another hidden form, where you could update any variabel, as long as the primary key is not connected as FK and changed

--- kid.php
+++ kid.php
@@ -64,7 +64,70 @@
 			echo "<span style='flex: 4;border: 1px solid red;'><input type='submit' value='Save!' /></span>";			
 			echo "</form>";
 		}
+?>
+
+<h3>Table With Hidden Input Detail Form</h3>
+
+<?php
+		if(isset($_POST['ModPNR'])){
+				$querystring='UPDATE kid SET PNR=:PNR, NAME=:NAME, BIRTHDAY=:BIRTHDAY, DISOBEDIENCE=:DISOBEDIENCE, DELIVERYNR=:DELIVERYNR, TYPE=:TYPE WHERE PNR=:MODPNR;';
+				$stmt = $pdo->prepare($querystring);
+				$stmt->bindParam(':PNR', $_POST['PNR']);
+				$stmt->bindParam(':NAME', $_POST['name']);
+				$stmt->bindParam(':BIRTHDAY', $_POST['birthday']);
+				$stmt->bindParam(':DISOBEDIENCE', $_POST['disobedience']);
+				$stmt->bindParam(':DELIVERYNR', $_POST['deliveryNr']);		
+                $stmt->bindParam(':TYPE', $_POST['type']);		
+                $stmt->bindParam(':MODPNR', $_POST['ModPNR']);	
+				$stmt->execute();				
+		}else if(isset($_POST['EdPNR'])){
+				echo "<div style='border:1px solid outset #888;border-radius:4px;background-color:#eee;'>";
+				echo "<form action='kid.php' method='post' >";
+                echo "<input type='hidden' name='ModPNR' value='".$_POST['EdPNR']."'>";
+                echo "PNR:<input type='text' name='PNR' value='".$_POST['EdPNR']."'><br>";
+                echo "name:<input type='text' name='name' value='".$_POST['name']."'><br>";
+                echo "birthday:<input type='text' name='birthday' value='".$_POST['birthday']."'><br>";
+                echo "disobedience:<input type='text' name='disobedience' value='".$_POST['disobedience']."'><br>";
+                echo "deliveryNr:<input type='text' name='deliveryNr' value='".$_POST['deliveryNr']."'><br>";
+                echo "type:<input type='text' name='type' value='".$_POST['type']."'><br>";
+                echo "<input type='submit' value='Save' >";			
+				echo "</form>";
+				echo "</div>";
+		}
+	
+		echo "<table style='border-collapse:collapse;'>";
+		foreach($pdo->query("SELECT * FROM kid") as $row){
+			if(isset($_POST['EdCustno'])){
+					if($_POST['EdCustno']==$row['CUSTNO']){
+								echo "<tr style='border:4px solid lightgreen;background-color:#448;color:#fff;box-shadow:2px 2px 2px #000;'>";
+					}else{
+								echo "<tr>";
+					}
+			}
+			
+			echo "<td>".$row['PNR']."</td>";					
+			echo "<td>".$row['name']."</td>";
+			echo "<td>".$row['birthday']."</td>";			
+			echo "<td>".$row['disobedience']."</td>";
+            echo "<td>".$row['deliveryNr']."</td>";
+            echo "<td>".$row['type']."</td>";
+			echo "<td>";
+				echo "<form action='kid.php' method='post' >";
+					echo "<input type='hidden' name='EdPNR' value='".$row['PNR']."'>";
+					echo "<input type='hidden' name='name' value='".$row['name']."'>";			
+					echo "<input type='hidden' name='birthday' value='".$row['birthday']."'>";			
+					echo "<input type='hidden' name='disobedience' value='".$row['disobedience']."'>";			
+                    echo "<input type='hidden' name='deliveryNr' value='".$row['deliveryNr']."'>";		
+                    echo "<input type='hidden' name='type' value='".$row['type']."'>";		
+					echo "<input type='submit' value='Edit' >";			
+				echo "</form>";
+			echo "</td>";
+			echo "</tr>";
+		}
+		echo "</table>"; 
 		
 ?>
+
+
 </body>
 </html>
